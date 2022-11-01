6c902209e2f706268718a63758dd1ce7a4e569a8
Tim Goodwin <timg.goodwin@gmail.com>
2022-11-01 12:35:28 -0700

    format

--- test-programs/src/timeofday.c
+++ test-programs/src/timeofday.c
@@ -1,15 +1,15 @@
-/* localtime example */
+/* nondeterministic time of day example */
 #include <stdio.h>
 #include <time.h>
 
-int main ()
+int main()
 {
   time_t rawtime;
-  struct tm * timeinfo;
+  struct tm *timeinfo;
 
-  time ( &rawtime );
-  timeinfo = localtime ( &rawtime );
-  printf ( "Current local time and date: %s", asctime (timeinfo) );
-  
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  printf("Current local time and date: %s", asctime(timeinfo));
+
   return 0;
 }
