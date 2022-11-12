fad8d5bfecba95d4d547e6c9a7a2758587f7f4f9
Tim Goodwin <timg.goodwin@gmail.com>
2022-11-12 15:30:06 -0800

    make record thread safe

--- lib/writer.c
+++ lib/writer.c
@@ -2,10 +2,14 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<pthread.h>
 
 #include "mjson.h"
 
 static const int MAX_VALUE_LEN = 512;
+
+int __log_idx_counter = 0;
+pthread_mutex_t idx_lock;
 
 struct logline {
     int id;
@@ -28,8 +32,7 @@ int parse_logline(const char* buf, struct logline *line) {
 
 // serializes a logline struct into a character buffer
 int gen_logline(struct logline *line, char* out) {
-    // TODO handle global id thread safe incrementation logic here.
-    return sprintf(out,
+    int status = sprintf(out,
             "{\"id\": %d, \"event_type\": %d, \"value\": \"%s\"}",
             line->id, line->event_type, line->value);
 };
@@ -93,59 +96,93 @@ struct logline* replay(int event_t, int last_read_idx) {
 };
 
 int record(int event_t, char* val) {
-    int id = 1;
-
+    pthread_mutex_lock(&idx_lock);
     FILE* fp;
-    fp = fopen("testlog.txt", "a");
+    fp = fopen("testlog2.txt", "a");
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
     struct logline *l = malloc(sizeof(struct logline));
-    l->id = id;
+    l->id = __log_idx_counter;
     l->event_type = event_t;
     strcpy(l->value, val);
 
+    // render log line into buffer then flush it out to disk
     char out[MAX_VALUE_LEN];
     gen_logline(l, out);
     fprintf(fp, "%s\n", out);
+
+    __log_idx_counter++;
+    pthread_mutex_unlock(&idx_lock);
     return 0;
 };
 
+void* trythis() {
+    char* val = "hey dude";
+    for(int i = 0; i < 10; i++) {
+        record(2, val);
+    }
+}
+
+int test_concurrent_record() {
+    pthread_t tid[2];
+
+    if (pthread_mutex_init(&idx_lock, NULL) != 0) {
+        printf("\n mutex init failed");
+        return EXIT_FAILURE;
+    }
+    int error;
+    for(int i = 0; i < 2; i++) {
+        error = pthread_create(&(tid[i]), NULL, &trythis, NULL);
+    }
+    if (error != 0) {
+        printf("failed to creat threads");
+    }
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    pthread_mutex_destroy(&idx_lock);
+}
+
 int main(int argc, char *argv[]) {
-    FILE* fp;
-    size_t linecap = 0;
-    ssize_t linelen;
-    char *line = NULL;
+    // FILE* fp;
+    // size_t linecap = 0;
+    // ssize_t linelen;
+    // char *line = NULL;
 
-    fp = fopen("testlog.txt", "r");
-    if (fp == NULL) {
-        exit(EXIT_FAILURE);
-    }
+    // fp = fopen("testlog.txt", "r");
+    // if (fp == NULL) {
+    //     exit(EXIT_FAILURE);
+    // }
 
-    int status;
-    while ((linelen = getline(&line, &linecap, fp)) > 0) {
-        struct logline *l = malloc(sizeof(struct logline));
-        status = parse_logline(line, l);
-        if (status == 0) {
-            char out[sizeof(struct logline)];
-            gen_logline(l, out);
-            puts(out);
-        } else {
-            printf("whoops: %s", json_error_string(status));
-        }
-        free(l);
-    }
+    // int status;
+    // while ((linelen = getline(&line, &linecap, fp)) > 0) {
+    //     struct logline *l = malloc(sizeof(struct logline));
+    //     status = parse_logline(line, l);
+    //     if (status == 0) {
+    //         char out[sizeof(struct logline)];
+    //         gen_logline(l, out);
+    //         puts(out);
+    //     } else {
+    //         printf("whoops: %s", json_error_string(status));
+    //     }
+    //     free(l);
+    // }
 
-    fclose(fp);
+    // fclose(fp);
 
-    if (line)
-        free(line);
+    // if (line)
+    //     free(line);
 
-    struct logline *ll = replay(2, 0);
-    puts(ll->value);
+    // struct logline *ll = replay(2, 0);
+    // puts(ll->value);
 
-    char *val = "hello";
-    record(2, val);
+    // char *val = "hello";
+    // record(2, val);
+    // record(2, val);
+    // record(2, val);
+    // record(2, val);
+    // record(2, val);
+    test_concurrent_record();
 
     exit(EXIT_SUCCESS);
 }
