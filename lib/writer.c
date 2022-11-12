5d4b8cf09b4ef35bf646366540a3beb672a5e03e
Tim Goodwin <timg.goodwin@gmail.com>
2022-11-12 14:11:48 -0800

    replay naive impl

--- lib/writer.c
+++ lib/writer.c
@@ -1,9 +1,9 @@
+#define _GNU_SOURCE
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
 
 #include "mjson.h"
-
 
 static const int MAX_VALUE_LEN = 512; 
 
@@ -26,37 +26,106 @@ int parse_logline(const char* buf, struct logline *line) {
     return json_read_object(buf, json_attrs, NULL);
 };
 
+// serializes a logline struct into a character buffer
 int gen_logline(struct logline *line, char* out) {
-    return sprintf(out, "{\"id\": %d, \"event_type\": %d, \"value\": \"%s\"}",
+    // TODO handle global id thread safe incrementation logic here.
+    return sprintf(out,
+            "{\"id\": %d, \"event_type\": %d, \"value\": \"%s\"}",
             line->id, line->event_type, line->value);
 };
 
-int main(int argc, char *argv[]) {
+int test_serialize(const char* buf) {
     struct logline *l = malloc(sizeof(struct logline));
-    int status = parse_logline(argv[1], l);
-    /* if (status == 0) { */
-    /*     printf("id: %d\n", l->id); */
-    /*     printf("event_type: %d\n", l->event_type); */
-    /*     printf("value: %s\n", l->value); */
-    /* } else { */
-    /*     puts(json_error_string(status)); */
-    /* } */
+    int status = parse_logline(buf, l);
+    if (status == 0) {
+        printf("id: %d\n", l->id);
+        printf("event_type: %d\n", l->event_type);
+        printf("value: %s\n", l->value);
+    } else {
+        puts(json_error_string(status));
+    }
+    // now serialize it back out
     char out[sizeof(struct logline)];
     gen_logline(l, out);
     puts(out);
+    free(l);
+    return 0;
+};
 
-    struct logline *l2 = malloc(sizeof(struct logline));
-    int status2 = parse_logline(out, l2);
-    if (status2 == 0) {
-        printf("id: %d\n", l2->id);
-        printf("event_type: %d\n", l2->event_type);
-        printf("value: %s\n", l2->value);
-    } else {
-        puts(json_error_string(status2));
+struct logline* replay(int event_t, int last_read_idx) {
+    FILE* fp;
+    size_t linecap = 0;
+    ssize_t linelen;
+    char *line = NULL;
+    
+    fp = fopen("testlog.txt", "r");
+    if (fp == NULL) {
+        exit(EXIT_FAILURE);
     }
-    l2->id = 2;
-    char out2[sizeof(struct logline)];
-    gen_logline(l2, out2);
-    puts(out2);
+
+    int curr_idx = 0;
+    int status = 0;
+    struct logline* result;
+
+    struct logline *found;
+
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
+        curr_idx++;
+        // skip over events we've seen already
+        if (curr_idx <= last_read_idx)
+            continue;
+
+        struct logline *l = malloc(sizeof(struct logline));
+        status = parse_logline(line, l);
+        if (status == 0) {
+            // skip over other event types
+            if (l->event_type != event_t) {
+                free(l);
+                continue;
+            }
+            // new record we havent seen yet thats of the event type we care about
+            found = l;
+        } else {
+            puts(json_error_string(status));
+        }
+    }
+    return found;
+};
+
+int main(int argc, char *argv[]) {
+    FILE* fp;
+    size_t linecap = 0;
+    ssize_t linelen;
+    char *line = NULL;
+
+    fp = fopen("testlog.txt", "r");
+    if (fp == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    int status;
+    while ((linelen = getline(&line, &linecap, fp)) > 0) {
+        struct logline *l = malloc(sizeof(struct logline));
+        status = parse_logline(line, l);
+        if (status == 0) {
+            char out[sizeof(struct logline)];
+            gen_logline(l, out);
+            puts(out);
+            puts("wrote it..");
+        } else {
+            printf("whoops: %s", json_error_string(status));
+        }
+        free(l);
+    }
+
+    fclose(fp);
+
+    if (line)
+        free(line);
+
+    struct logline *ll = replay(2, 0);
+    puts(ll->value);
+
+    exit(EXIT_SUCCESS);
 }
 
