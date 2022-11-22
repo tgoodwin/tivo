bd278542971070163173f1e3b87fb29cfeadd005
Tim Goodwin <timg.goodwin@gmail.com>
2022-11-21 23:36:28 -0800

    rr_ctx struct

--- lib/writer.h
+++ lib/writer.h
@@ -7,10 +7,16 @@
 #define EXEC_MODE_REPLAY 1
 
 struct logline {
-    int id;                     // used as a serial index
-    int writer_id;              // modeling this as an int for now
-    int event_type;             // modeling this as an int for now
-    char value[MAX_VALUE_LEN];  // a character sequence for now. generalize later if needed
+    int id;                         // used as a serial index
+    int writer_id;                  // modeling this as an int for now
+    int event_type;                 // modeling this as an int for now
+    char value[MAX_VALUE_LEN];      // a character sequence for now. generalize later if needed
+};
+
+/* thread-local information for replay */
+struct rr_ctx {
+    int writer_id;
+    int offsets_by_type[256]; // idx: event_t, val: last read idx
 };
 
 extern int record(int writer_id, int event_t, char *val);
