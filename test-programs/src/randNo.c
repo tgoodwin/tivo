befe44182fa98937caed1bd23904a37543de5f78
Ananya Das <34390803+ananyadas2607@users.noreply.github.com>
2022-11-28 12:03:39 -0800

    Changed randNo.c and ll-Ananya

--- test-programs/src/randNo.c
+++ test-programs/src/randNo.c
@@ -1,30 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-unsigned int MyRand(unsigned int start_range,unsigned int end_range)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
 {
-    static unsigned int rand = 0xACE1U; 
-
-    if(start_range == end_range) {
-        return start_range;
-    }
-
-    rand += 0x3AD;
-    rand %= end_range;
-    
-    while(rand < start_range){
-        rand = rand + end_range - start_range;
-    }
-
-    return rand;
-}
-
-int main(void)
-{
-    int i;
-    for (i = 0; i < 0xFF; i++)
-    {
-        printf("%u\t",MyRand(10,20));
-    }
-    return 0;
+int rand_num;
+srand(time(0));
+printf("The randomly generated number is ");
+rand_num = rand();
+printf("%d\n", rand_num);
 }
