1f8caee5ef0e02027db975b41928a32a6ad46910
Ananya Das <34390803+ananyadas2607@users.noreply.github.com>
2022-11-29 12:08:10 -0800

    Ananya Commit

--- test-programs/src/cas.c
+++ test-programs/src/cas.c
@@ -10,7 +10,17 @@ bool __tivo_sync_bool_compare_and_swap_4(volatile int32_t* data,
   return result;
 }
 
+void add10(int* x){
+  int expected=*x;
+   while (__sync_bool_compare_and_swap_4(x, expected, expected + 10)
+            != expected) {
+        expected = *x;
+        }
+}
+
 int main(int argc, char** argv) {
   int32_t data, expected, desired;
   int a = __sync_bool_compare_and_swap_4(&data, expected, desired);
+
+  add10(5);
 }
